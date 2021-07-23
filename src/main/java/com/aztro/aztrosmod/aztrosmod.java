8a7fe1184ed176f04c479f1b6a197ab0d5645a9f
distortiondev <68188829+distortiondev@users.noreply.github.com>
2021-07-23 01:09:10 -0400

    ores drop properly

--- src/main/java/com/aztro/aztrosmod/aztrosmod.java
+++ src/main/java/com/aztro/aztrosmod/aztrosmod.java
@@ -4,6 +4,8 @@ import com.aztro.aztrosmod.registry.ModBlocks;
 import com.aztro.aztrosmod.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
+import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
@@ -17,11 +19,24 @@ public class aztrosmod implements ModInitializer {
             () -> new ItemStack(ModItems.CUT_DIAMOND_BLOCK)
     );
 
+    private static final Identifier DIAMOND_ORE_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/diamond_ore");
+    private static final Identifier DEEPSLATE_DIAMOND_ORE_LOOT_TABLE_ID = new Identifier("minecraft", "blocks/deepslate_diamond_ore");
+    private static final Identifier CUT_DIAMOND_BLOCK_LOOT_TABLE_ID = new Identifier(MOD_ID, "blocks/cut_diamond_block");
+
     @Override
     public void onInitialize() {
         System.out.println("Hello, Minecraft!");
         ModItems.registerItems();
         ModBlocks.registerBlocks();
     }
+
+//THIS IS NOT NEEDED RIGHT NOW BUT MAY HELP COMPATIBILITY LATER ON
+//    private void modifyLootTables() {
+//        LootTableLoadingCallback.EVENT.register(((resourceManager, manager, id, supplier, setter) -> {
+//            if (DIAMOND_ORE_LOOT_TABLE_ID.equals(id)) {
+//                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder().rolls(ConstantLootTableRange.create())
+//            }
+//        }));
+//    }
 
 }
