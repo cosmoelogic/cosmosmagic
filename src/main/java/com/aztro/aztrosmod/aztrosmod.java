b918f23c5d352802f7ce53ada633f1d483047390
distortiondev <68188829+distortiondev@users.noreply.github.com>
2021-07-23 00:36:56 -0400

    Added Crude Diamonds

--- src/main/java/com/aztro/aztrosmod/aztrosmod.java
+++ src/main/java/com/aztro/aztrosmod/aztrosmod.java
@@ -3,10 +3,19 @@ package com.aztro.aztrosmod;
 import com.aztro.aztrosmod.registry.ModBlocks;
 import com.aztro.aztrosmod.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class aztrosmod implements ModInitializer {
 
     public static final String MOD_ID = "aztros";
+
+    public static final ItemGroup ITEM_GROUP = FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "general"),
+            () -> new ItemStack(ModItems.CUT_DIAMOND_BLOCK)
+    );
 
     @Override
     public void onInitialize() {
--- src/main/java/com/aztro/aztrosmod/registry/ModItems.java
+++ src/main/java/com/aztro/aztrosmod/registry/ModItems.java
@@ -7,12 +7,17 @@ import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item CUT_DIAMOND = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    //cut diamond stuff
+    public static final Item CUT_DIAMOND = new Item(new Item.Settings().group(aztrosmod.ITEM_GROUP));
+    public static final Item CUT_DIAMOND_BLOCK = new BlockItem(ModBlocks.CUT_DIAMOND_BLOCK, new Item.Settings().group(aztrosmod.ITEM_GROUP));
 
-    public static final Item CUT_DIAMOND_BLOCK = new BlockItem(ModBlocks.CUT_DIAMOND_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    //crude diamond
+    public static final Item CRUDE_DIAMOND = new Item(new Item.Settings().group(aztrosmod.ITEM_GROUP));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(aztrosmod.MOD_ID, "cut_diamond"), CUT_DIAMOND);
         Registry.register(Registry.ITEM, new Identifier(aztrosmod.MOD_ID, "cut_diamond_block"), CUT_DIAMOND_BLOCK);
+
+        Registry.register(Registry.ITEM, new Identifier(aztrosmod.MOD_ID, "crude_diamond"), CRUDE_DIAMOND);
     }
 }
