b835df02556430e31fd895e622e8b094c145027e
michael <mickgober@gmail.com>
2021-08-08 21:58:22 -0400

    added OS detection to ConfigManager

--- src/main/java/com/aztro/aztrosmod/ConfigManager.java
+++ src/main/java/com/aztro/aztrosmod/ConfigManager.java
@@ -2,6 +2,10 @@ package com.aztro.aztrosmod;
 
 public class ConfigManager {
     public static void Manage() {
-        System.out.println(System.getenv("APPDATA"));
+        if ("Windows".equals(System.getProperty("os.name"))) {
+            System.out.println(System.getenv("APPDATA"));
+        } else {
+            System.out.println(System.getProperty("user.home"));
+        }
     }
 }
